58e7b393ffd20f07690323486987ddce08600869
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-12-08 13:45:54 +0800

    Delete en_readability directory

--- en_readability/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Scores the readability of a text and output a variety of metrics including: Flesch-Kincaid Grade Level, Gunning Fog, ARI, Dale Chall, SMOG, and more
-"""
-
-from .en_readability import Readability
